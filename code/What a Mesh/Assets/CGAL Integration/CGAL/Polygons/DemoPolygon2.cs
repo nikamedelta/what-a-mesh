c934af06126057f547b17deed64bb29e159559dd
s378189 <annika.meess@stud-mail.uni-wuerzburg.de>
2020-11-26 09:57:49 +0100

    Unity project setup with gitignore and CGAL integration (3rd party, from https://github.com/andybak/CGALUnity)

---
+++ code/What a Mesh/Assets/CGAL Integration/CGAL/Polygons/DemoPolygon2.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Common.Core.LinearAlgebra;
+using Common.Unity.Drawing;
+using CGAL.Polygons;
+
+namespace CGALDemo
+{
+
+    public class DemoPolygon2 : Polygon2Input
+    {
+
+        private class IntersectionResult
+        {
+            public bool contains;
+            public Vector2f point;
+        }
+
+        Polygon2f polygon;
+
+        IntersectionResult result;
+
+        protected override void OnPolygonComplete(Polygon2f input)
+        {
+            polygon = input;
+            polygon.BuildIndices();
+            polygon.BuildHoleIndices();
+            result = null;
+        }
+
+        protected override void OnPolygonCleared()
+        {
+            result = null;
+        }
+
+        protected override void OnLeftClick(Vector2f point)
+        {
+            if (MadePolygon)
+            {
+                PolygonIntersection2.PushPolygon(polygon);
+
+                result = new IntersectionResult();
+                result.contains = PolygonIntersection2.ContainsPoint(point);
+                result.point = point;
+
+                PolygonIntersection2.PopPolygon();
+
+            }
+        }
+
+        protected override void OnPostRender()
+        {
+
+            if (!MadePolygon)
+            {
+                base.OnPostRender();
+            }
+            else
+            {
+                Camera cam = Camera.current;
+                if (cam == null) return;
+
+                DrawPolygon(cam, polygon, Color.green, Color.yellow);
+            }
+
+        }
+
+        protected void OnGUI()
+        {
+            int textLen = 400;
+            int textHeight = 25;
+
+            if (!MadePolygon)
+            {
+                GUI.Label(new Rect(10, 10, textLen, textHeight), "Space to clear polygon.");
+                GUI.Label(new Rect(10, 30, textLen, textHeight), "Left click to place point.");
+                GUI.Label(new Rect(10, 50, textLen, textHeight), "Click on first point to close polygon.");
+            }
+            else
+            {
+                GUI.Label(new Rect(10, 10, textLen, textHeight), "Space to clear polygon.");
+                GUI.Label(new Rect(10, 30, textLen, textHeight), "Polygon is Simple = " + polygon.IsSimple);
+                GUI.Label(new Rect(10, 50, textLen, textHeight), "Polygon is Convex = " + polygon.IsConvex);
+                GUI.Label(new Rect(10, 70, textLen, textHeight), "Polygon area = " + polygon.Area);
+                GUI.Label(new Rect(10, 90, textLen, textHeight), "Polygon Orientation = " + polygon.Orientation);
+
+                GUI.Label(new Rect(10, 120, textLen, textHeight), "Left click to test intersection.");
+
+                if(result != null)
+                {
+                    GUI.Label(new Rect(10, 140, textLen, textHeight), string.Format("Point {0},{1} is {2}.", 
+                        result.point.x, result.point.y, result.contains));
+                }
+            }
+            
+        }
+
+
+    }
+}
---
+++ code/What a Mesh/Assets/CGAL Integration/CGAL/Polygons/DemoPolygonPartition2.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Common.Core.LinearAlgebra;
+using Common.Unity.Drawing;
+using CGAL.Polygons;
+
+namespace CGALDemo
+{
+
+    public class DemoPolygonPartition2 : Polygon2Input
+    {
+
+        List<Polygon2f> partition;
+
+        Polygon2f polygon;
+
+        private void Start()
+        {
+            SnapPoint = 10.0f;
+        }
+
+        protected override void OnPolygonComplete(Polygon2f input)
+        {
+            polygon = input;
+
+            polygon.MakeCCW();
+            polygon.BuildIndices();
+            polygon.BuildHoleIndices();
+
+            if (polygon.IsSimple && !polygon.HasHoles)
+            {
+                partition = PolygonPartition2.Partition(polygon, PARTITION_METHOD.OPTIMAL);
+            }
+            else
+            {
+                partition = new List<Polygon2f>();
+            }
+
+            foreach(var poly in partition)
+            {
+                poly.MakeCCW();
+                poly.BuildIndices();
+                poly.BuildHoleIndices();
+            }
+
+        }
+
+        protected void OnGUI()
+        {
+            int textLen = 400;
+            int textHeight = 25;
+
+            if (!MadePolygon)
+            {
+                GUI.Label(new Rect(10, 10, textLen, textHeight), "Space to clear polygon.");
+                GUI.Label(new Rect(10, 30, textLen, textHeight), "Left click to place point.");
+                GUI.Label(new Rect(10, 50, textLen, textHeight), "Click on first point to close polygon.");
+            }
+            else
+            {
+                GUI.Label(new Rect(10, 10, textLen, textHeight), "Space to clear polygon.");
+
+                if(!polygon.IsSimple || polygon.HasHoles)
+                {
+                    GUI.Label(new Rect(10, 30, textLen, textHeight), "Polygon must be simple with no holes. Clear and start again.");
+                }
+                else if (polygon.Orientation != ORIENTATION.COUNTERCLOCKWISE)
+                {
+                    GUI.Label(new Rect(10, 30, textLen, textHeight), "Polygon must have ccw orientation. Clear and start again.");
+                }
+                else
+                {
+                    GUI.Label(new Rect(10, 30, textLen, textHeight), "Partitions = " + partition.Count);
+                }
+            }
+
+        }
+
+        protected override void OnPostRender()
+        {
+            if (!MadePolygon)
+            {
+                base.OnPostRender();
+            }
+            else
+            {
+                Camera cam = Camera.current;
+                if (cam == null) return;
+
+                DrawPolygon(cam, polygon, Color.green, Color.yellow);
+
+                foreach (var polygon in partition)
+                    DrawPolygon(cam, polygon, Color.green, Color.yellow);
+            }
+        }
+
+    }
+}
