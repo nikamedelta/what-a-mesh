9eb9af0e4b8883ce468513b0667f5b87c5f48ff4
Cazo0 <Janderkleine98@gmail.com>
2020-12-07 15:52:15 +0100

    made selected Vertex to kind of follow mouse, only works on x,y plane

--- code/What a Mesh/Assets/Scripts/moveVertices.cs
+++ code/What a Mesh/Assets/Scripts/moveVertices.cs
@@ -14,85 +14,87 @@ public class moveVertices : MonoBehaviour
 
 
     int indexToMove = -1;
+    bool foundVertex;
     
     void Start()
     {
-        mesh = GetComponent<MeshFilter>().mesh;
-        vertices = mesh.vertices;
-        normals = mesh.normals;
-        Debug.Log(vertices.Length);
         cam = Camera.main;
-
-        for (int i = 0; i<vertices.Length; i++)
-        {
+        //for (int i = 0; i<vertices.Length; i++)
+        //{
             //Debug.DrawLine(vertices[i] + transform.position, normals[i] + transform.position, Color.green, float.MaxValue);
-        }
+        //}
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
-            mStart = Input.mousePosition;
-            RaycastHit hit;
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit, 300))
+            if (!foundVertex)
             {
-                indexToMove = FindNearestPoint(hit.point);
-                Debug.Log("nearest Point is " + vertices[indexToMove] + " (" + indexToMove + ")");
+                mStart = Input.mousePosition;
+                RaycastHit hit;
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit, 300))
+                {
+                    indexToMove = FindNearestVertex(hit.point, hit.transform.gameObject);
+                    foundVertex = true;
+                    Debug.Log("nearest Point is " + vertices[indexToMove] + " (" + indexToMove + ")");
+                }
             }
 
-            //Debug.DrawLine(ray.origin, hit.point, Color.green, float.MaxValue);
-            Debug.Log("Ray hit at " + hit.point);
-
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            if (indexToMove != -1)
+            else
             {
-                mEnd = Input.mousePosition;
-
+                mEnd = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z + vertices[indexToMove].z);
                 // calculate length for change
-                Ray screenRay = cam.ScreenPointToRay(Input.mousePosition);
-                
+                //Ray screenRay = cam.ScreenPointToRay(Input.mousePosition);
+                //mEnd = cam.ScreenToWorldPoint(Input.mousePosition);
+
                 // two methods to change vertex, choose one!
-                Vector3 dir = normals[indexToMove].normalized;
-                //Vector3 dir = (mEnd-mStart).normalized;
+                //Vector3 dir = normals[indexToMove].normalized;
+                Vector3 dir = (mEnd - mStart).normalized;
 
 
                 vertices[indexToMove] = dir;
-
-
 
                 // reassign values to object's mesh
                 mesh.vertices = vertices;
                 mesh.normals = normals;
                 mesh.RecalculateNormals();
                 mesh.RecalculateBounds();
-
             }
-            // reset index
+
+            //Debug.DrawLine(ray.origin, hit.point, Color.green, float.MaxValue);
+            //Debug.Log("Ray hit at " + hit.point);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log(mEnd);
+            foundVertex = false;
             indexToMove = -1;
         }
     }
 
-    private int FindNearestPoint(Vector3 point)
+    private int FindNearestVertex(Vector3 point, GameObject obj)
     {
+        mesh = obj.GetComponent<MeshFilter>().mesh;
+        vertices = mesh.vertices;
+        normals = mesh.normals;
+        Debug.Log(vertices.Length);
         int index = -1;
         float shortestDistance = float.MaxValue;
 
         for (int i = 0; i < vertices.Length; i++)
         {
-            float distance = Vector3.Distance(point, transform.position + vertices[i]);
+            float distance = Vector3.Distance(point, obj.transform.position + vertices[i]);
             if (distance < shortestDistance)
             {
                 index = i;
                 shortestDistance = distance;
             }
         }
-
+        Debug.Log(vertices[index]);
         return index;
     }
 
