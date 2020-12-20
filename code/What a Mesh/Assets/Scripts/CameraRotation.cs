ddad40d638df83655930a8e3ab928312869c51e6
s378189 <annika.meess@stud-mail.uni-wuerzburg.de>
2020-12-20 18:35:54 +0100

    small Showcase Scene for presentation

---
+++ code/What a Mesh/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+     
+     public float Speed = 0.1f;
+
+    void Update()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+        float zAxisValue = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+
+        transform.position = new Vector3(transform.position.x + xAxisValue, transform.position.y, transform.position.z + zAxisValue);
+
+    }
+
+
+}
+
